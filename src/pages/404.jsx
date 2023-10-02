df1c7d46e08f8279ba983b5be2320247760e581e
Билгүүн Баттөгс <LP230078@G301-CC0173.local>
2023-10-02 18:53:27 +0800

    upgrade v3 contact page added

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import Button from "@mui/material/Button";
+import Link from "next/link";
 
 const ErrorPage = () => {
   return (
     <main className="w-1/2 flex mx-auto h-[800px] items-center">
-      <div className="w-1/2 text-center text-6xl">
+      <div className="w-1/2 h-56 flex justify-center items-center text-6xl border-r-[1px] mr-9">
         <h1>404</h1>
       </div>
       <div className="w-1/2">
-        <h1 className="text-2xl font-medium mb-10">Page Not Found</h1>
-        <p className="text-secondary500 font-light">
+        <h1 className="text-2xl font-medium">Page Not Found</h1>
+        <p className="text-secondary500 font-light my-5">
           We're sorry. This Page is unknown or does not exist the page you are
           looking for
         </p>
-        <Button color="secondary" variant="contained">
-          Back To Home
-        </Button>
+        <Link href="../">
+          <Button className="bg-blue-600 " variant="contained">
+            Back To Home
+          </Button>
+        </Link>
       </div>
     </main>
   );
---
+++ src/pages/contact/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <main className="flex flex-col items-center">
+      <div className="mt-24 w-5/6">
+        <h1>Contact Us</h1>
+      </div>
+    </main>
+  );
+};
+
+export default Contact;
